dc48ef9cb1f79afb791b2b73791e765ad81f2ca0
Pilakkal Insaf <insafpilakkal@gmail.com>
2022-03-04 19:40:48 +0530

    contact and upadates details pages added

---
+++ contact.php
@@ -0,0 +1,171 @@
+<?php
+$page='contact.php';
+require('inc/header.php'); 
+require('inc/nav.php');  
+?>
+
+<main>
+
+<!-- Let's Talk -->
+
+<section class="lets-talk pt pb">
+    <div class="container">
+        <div class="row">
+            <div class="col-sm-6">
+                <h2>Let's talk</h2>
+                <p>Ask us anything or just say hai..</p>
+                <svg viewBox="0 0 12 85" fill="none" xmlns="http://www.w3.org/2000/svg">
+                    <path d="M5.46967 84.5303C5.76256 84.8232 6.23743 84.8232 6.53033 84.5303L11.3033 79.7574C11.5962 79.4645 11.5962 78.9896 11.3033 78.6967C11.0104 78.4038 10.5355 78.4038 10.2426 78.6967L6 82.9393L1.75736 78.6967C1.46446 78.4038 0.989589 78.4038 0.696696 78.6967C0.403802 78.9896 0.403802 79.4645 0.696696 79.7574L5.46967 84.5303ZM5.25 -3.27835e-08L5.25 84L6.75 84L6.75 3.27835e-08L5.25 -3.27835e-08Z" fill="#C0C0C0"/>
+                </svg>
+            </div>
+            <div class="col-sm-6 form-box">
+                <h4>Get in touch!</h4>
+                <form action="">
+
+                    <div class="form-group">
+                        <textarea name="" id=""rows="0" placeholder="Message HERE!"></textarea>
+
+                    </div>
+
+                    <div class="form-group">
+                        <input type="message" name="message" placeholder="Name">
+                    </div>
+                    <div class="form-group">
+                        <input type="text" name="messaga" placeholder="Comapny">
+                    </div>
+                    <div class="form-group">
+                        <input type="text" name="messaga" placeholder="Phone/email">
+                    </div>
+                    <button name="submit" type="submit" class="secondary-btn" value="Send Message">Get a Quote</button>
+                </form>
+            </div>
+        </div>
+    </div>
+</section>
+
+
+
+<!-- Office info -->
+
+<section class="office-info">
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col-sm-6 align-self-sm-center info-wrapper">
+                <div class="address-info">
+                    <svg width="15" height="15" viewBox="0 0 15 15" fill="none" xmlns="http://www.w3.org/2000/svg">
+                        <path d="M7.42709 5.10382C8.98896 5.10382 9.65515 5.77014 9.65515 7.3323H11.1405C11.1405 4.93669 9.82225 3.61817 7.42709 3.61817V5.10382ZM9.96856 9.14703C9.82585 9.01729 9.63832 8.9481 9.44557 8.95406C9.25281 8.96002 9.06991 9.04067 8.93549 9.17897L7.15823 11.0071C6.73045 10.9254 5.87041 10.6572 4.98513 9.77397C4.09985 8.88778 3.83174 8.02536 3.75227 7.60046L5.57854 5.82214C5.71698 5.68778 5.79773 5.50482 5.80369 5.31197C5.80965 5.11912 5.74036 4.93152 5.61047 4.78887L2.86624 1.77076C2.7363 1.62766 2.55571 1.54086 2.36281 1.52879C2.16992 1.51673 1.97992 1.58034 1.83316 1.70614L0.22153 3.08854C0.093128 3.21743 0.0164888 3.38897 0.00615091 3.57063C-0.0049894 3.75634 -0.217398 8.15535 3.19302 11.5679C6.16823 14.5429 9.89504 14.7606 10.9214 14.7606C11.0715 14.7606 11.1635 14.7561 11.1881 14.7546C11.3697 14.7445 11.5411 14.6675 11.6693 14.5385L13.0507 12.9258C13.1766 12.7791 13.2403 12.5891 13.2284 12.3962C13.2165 12.2033 13.1298 12.0226 12.9868 11.8925L9.96856 9.14703Z" fill="#319CDD"/>
+                        <path d="M13.259 6.68467H14.7443C14.7443 2.87398 11.8679 0 8.05273 0V1.48565C11.0695 1.48565 13.259 3.67178 13.259 6.68467Z" fill="#319CDD"/>
+                        </svg>
+                    <p><a href="">info@pmkbuldtech.in</a></p> 
+                </div>
+                <div class="address-info">
+                    <svg width="17" height="16" viewBox="0 0 17 16" fill="none" xmlns="http://www.w3.org/2000/svg">
+                        <path d="M15.3 16H1.7C1.24913 16 0.81673 15.8286 0.497918 15.5235C0.179107 15.2183 0 14.8045 0 14.373V5.46416C0.01413 5.19524 0.0967123 4.93371 0.24056 4.70232C0.384407 4.47093 0.585161 4.27668 0.82535 4.1365L7.62535 0.231624C7.88946 0.0800582 8.19163 0 8.49958 0C8.80752 0 9.10969 0.0800582 9.3738 0.231624L16.1738 4.1365C16.6847 4.43181 16.9983 4.96059 17 5.53168V14.373C17 14.8045 16.8209 15.2183 16.5021 15.5235C16.1833 15.8286 15.7509 16 15.3 16ZM1.7 6.13043V14.373H15.3V6.13043L8.5 10.4689L1.7 6.13043ZM8.5 1.6268L2.8186 4.889L8.5 8.51403L14.1806 4.889L8.5 1.6268V1.6268Z" fill="#319CDD"/>
+                        </svg>
+                        <p><a href="#">+91 9847 648 648</a></p>                        
+                </div> 
+                <div class="address-info">
+                    <svg width="14" height="19" viewBox="0 0 14 19" fill="none" xmlns="http://www.w3.org/2000/svg">
+                        <path d="M6.89129 19C5.82015 17.9493 4.82086 16.8254 3.9004 15.6361C2.81688 14.2483 1.83518 12.7951 1.07467 11.1956C0.594787 10.185 0.210877 9.14119 0.0647189 8.02054C-0.151108 6.37154 0.173363 4.8291 1.03083 3.41999C2.16258 1.56332 3.81027 0.44614 5.92957 0.0907643C8.73143 -0.380096 11.603 1.01365 13.0285 3.51466C14.149 5.48285 14.2708 7.54175 13.5454 9.674C13.0046 11.2645 12.1837 12.7029 11.2439 14.0758C10.0746 15.7848 8.74361 17.3579 7.29323 18.826C7.23379 18.886 7.17094 18.942 7.11004 19H6.89129ZM3.79176 7.13731C3.79176 8.93153 5.23824 10.3937 7.01455 10.3922C8.76358 10.3922 10.2071 8.9236 10.2096 7.14524C10.212 5.34358 8.78356 3.86607 7.03209 3.86607C6.60756 3.86207 6.18647 3.94377 5.79318 4.10643C5.3999 4.2691 5.04223 4.5095 4.74088 4.81373C4.43952 5.11796 4.20047 5.47996 4.03756 5.87881C3.87464 6.27765 3.7911 6.7054 3.79176 7.13731V7.13731Z" fill="#319CDD"/>
+                        </svg>
+                        
+                        
+                    <p>Near Azhathrikovil Temple, NH Bypass Thondayad, Nellikode PO, Kozhikode, Kerala 673 016</p>                        
+                </div>
+            </div>
+
+            <div class="col-sm-6 map-info">
+               <div class="map">
+                <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3913.018382613303!2d75.77543021488931!3d11.260057791994448!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3ba65930cf2d36ed%3A0x48cc7db9e2f534be!2sPMK%20Constructions%20India%20Private%20Limited!5e0!3m2!1sen!2sin!4v1645766624784!5m2!1sen!2sin" width="100%" height="600" style="border:0;" allowfullscreen="" loading="lazy"></iframe>
+               </div>
+            </div>
+        </div>
+    </div>
+</section>
+
+
+<!-- career -->
+
+<section class="career">
+        <div class="container-fluid">
+            <div class="row">
+                <div class="col-sm-6">
+                    <img src="images/career.jpg" class="img-fluid" alt="">
+                </div>
+                <div class="col-sm-6 align-self-sm-center ">
+                    <h2>Careers at PMK</h2>
+                    <p>Lorem Ipsum is simply dummy text of the printing <a href="">info@pmkbuldtech.in</a> </p>
+                </div>
+            </div>
+        </div>
+</section>
+
+
+<!-- Cta Project -->
+
+<section class="project-cta">
+    <div class="container">
+        <div class=" row">
+            <div class="col-sm pt pb">
+                <span class="small-heading">Continue to</span>
+                <h2>Our Project</h2>
+            </div>
+            <div class="col-sm d-flex justify-content-end">
+                <svg class="continue-icon" viewBox="0 0 38 38" fill="none" xmlns="http://www.w3.org/2000/svg">
+                    <circle cx="19" cy="19" r="18.5" stroke="#319CDD"/>
+                    <path d="M28.7071 19.7071C29.0976 19.3166 29.0976 18.6834 28.7071 18.2929L22.3431 11.9289C21.9526 11.5384 21.3195 11.5384 20.9289 11.9289C20.5384 12.3195 20.5384 12.9526 20.9289 13.3431L26.5858 19L20.9289 24.6569C20.5384 25.0474 20.5384 25.6805 20.9289 26.0711C21.3195 26.4616 21.9526 26.4616 22.3431 26.0711L28.7071 19.7071ZM10 20H28V18H10V20Z" fill="#319CDD"/>
+                    </svg>
+                    
+            </div>
+    </div>
+    </div>
+</section>
+
+  <!-- Upadates -->
+<!--     
+  <section class="updates">
+    <div class="container-fluid">
+        <div class="row news-updates pt">
+                <div class="col-sm-4">
+                    <h2>Important Updates</h2>
+                </div>
+                <div class="col-sm latest-news">
+                    <img src="images/updates/updates-01.jpg" alt="">
+                       <div class="caption-overlay">
+                         <div class="caption">
+                             <h3>Lorem Ipsum is simply dummy text</h3>
+                         </div>
+                       </div>     
+                </div>
+                <div class="col-sm latest-news">
+                 <img src="images/updates/updates-02.jpg" alt="">
+                 <div class="caption-overlay">
+                     <div class="caption">
+                         <h3>Lorem Ipsum is simply dummy text</h3>
+                     </div>
+                   </div> 
+             </div>
+             <div class="col-sm latest-news">
+                 <img src="images/updates/updates-03.jpg" alt="">
+                 <div class="caption-overlay">
+                     <div class="caption">
+                         <h3>Lorem Ipsum is simply dummy text</h3>
+                     </div>
+                   </div> 
+             </div>
+             
+        </div>
+    </div>
+</section> -->
+
+</main>
+
+<?php 
+
+require('inc/footer.php');
+require('inc/script.php');
+
+?>
+
+</body>
+</html>
---
+++ updates-details.php
@@ -0,0 +1,146 @@
+
+<?php 
+$page='project.php';
+require('inc/header.php'); 
+require('inc/nav.php');  
+?>
+
+
+<main>
+
+<!-- upadates banner -->
+
+<section class="updates-details hero-banner">
+    <div class="container-fluid">
+     <div class="updates-banner">
+         <img src="images/updates/updates-details.jpg" alt="">
+         <div class="overlay">
+             <h3 class="updates-details-heading">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Hic asperiores explicabo architecto dolore nulla esse.</h3>   
+         </div>
+          <div class="scroll-down"><i class="fa-solid fa-arrow-down-long"></i></div>
+    </div>
+    </div>
+</section>
+
+<section class="details-description scroll-to pt pb">
+    <div class="continer">
+
+        <svg viewBox="0 0 512 680" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <rect width="512" height="680" fill="#FDF9F4"/>
+            </svg>
+            
+        <div class="row">
+            <div class="col-sm-6 updates-des-mobile">
+                <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
+            <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in </p>
+            <hr>
+            </div>
+            <div class="col-sm-6 pad-left">
+                <div class="recent-news">
+                    <img src="images/updates/recent-news01.jpg" alt="">
+                    <div class="recent-cta">
+                        <h4>Lorem Ipsum is simply dummy text </h4>
+                        <a href="">know More<i class="fa-solid fa-arrow-right-long"></i></a>
+                    </div>
+                </div>
+                <div class="recent-news">
+                    <img src="images/updates/recent-news01.jpg" alt="">
+                    <div class="recent-cta">
+                        <h4>Lorem Ipsum is simply dummy text </h4>
+                        <a href="">know More<i class="fa-solid fa-arrow-right-long"></i></a>
+                    </div>
+                </div>
+                <div class="recent-news">
+                    <img src="images/updates/recent-news01.jpg" alt="">
+                    <div class="recent-cta">
+                        <h4>Lorem Ipsum is simply dummy text </h4>
+                        <a href="">know More<i class="fa-solid fa-arrow-right-long"></i></a>
+                    </div>
+                </div>
+            </div>
+        </div>
+        
+            
+    </div>
+
+</section>
+
+   <!-- Testimonials -->
+
+   <section class="testimonial m-0">
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col-sm-8">
+                <div class="testimonials owl-carousel owl-theme">
+                    <div class="item">
+                        <img class="review-img" src="images/testimonial/demo.jpg" alt="">
+                        <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release.</p>
+                        <h4>John David</h4>
+                    </div>
+                    <div class="item">
+                        <img class="review-img" src="images/testimonial/demo.jpg" alt="">
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quia accusantium obcaecati maiores amet. Blanditiis veniam consectetur nisi. Sit quos suscipit obcaecati saepe perferendis id, vel necessitatibus ullam quo sint placeat magni aperiam explicabo, natus est provident nesciunt quisquam ipsum nihil molestiae porro a reiciendis reprehenderit? Voluptatem eligendi iste veniam ipsum eum ea voluptas nobis, natus aspernatur aperiam recusandae maxime minima repudiandae, facere doloremque</p>
+                        <h4>John David</h4>
+                    </div>
+                </div>
+            </div>
+            <div class="col-sm-4 testimonial-heading align-middle">
+                <svg width="59" height="49" viewBox="0 0 59 49" fill="none" xmlns="http://www.w3.org/2000/svg">
+                    <path d="M22.4552 25.9677V49H0V32.7419C0 26.4194 1.02736 20.6237 3.08209 15.3548C5.13682 10.086 8.58582 4.96774 13.4291 0L21.7948 6.77418C16.0709 13.2473 12.842 19.6452 12.1082 25.9677H22.4552ZM59 25.9677V49H36.5448V32.7419C36.5448 26.4194 37.5721 20.6237 39.6269 15.3548C41.6816 10.086 45.1306 4.96774 49.9739 0L58.3396 6.77418C52.6157 13.2473 49.3868 19.6452 48.653 25.9677H59Z" fill="#77BBE5"/>
+                    </svg>
+                    
+                <h2>What Our clients say</h2>
+            </div>
+        </div>
+    </div>
+</section>
+
+  <!-- Upadates -->
+    
+  <!-- <section class="updates">
+    <div class="container-fluid">
+        <div class="row news-updates pt">
+                <div class="col-sm-4">
+                    <h2>Important Updates</h2>
+                </div>
+                <div class="col-sm latest-news">
+                    <img src="images/updates/updates-01.jpg" alt="">
+                       <div class="caption-overlay">
+                         <div class="caption">
+                             <h3>Lorem Ipsum is simply dummy text</h3>
+                         </div>
+                       </div>     
+                </div>
+                <div class="col-sm latest-news">
+                 <img src="images/updates/updates-02.jpg" alt="">
+                 <div class="caption-overlay">
+                     <div class="caption">
+                         <h3>Lorem Ipsum is simply dummy text</h3>
+                     </div>
+                   </div> 
+             </div>
+             <div class="col-sm latest-news">
+                 <img src="images/updates/updates-03.jpg" alt="">
+                 <div class="caption-overlay">
+                     <div class="caption">
+                         <h3>Lorem Ipsum is simply dummy text</h3>
+                     </div>
+                   </div> 
+             </div>
+             
+        </div>
+    </div>
+</section> -->
+
+</main>
+
+
+<?php 
+
+require('inc/footer.php');
+require('inc/script.php');
+
+?>
+
+</body>
+</html>
